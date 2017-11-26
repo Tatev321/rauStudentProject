2909b93ed1da1a553c24378c74f105b11e59f85a
Tatev Soghomonyan <tatev9721@gmail.com>
2017-11-26 16:15:48 +0400

    All Done

--- .history/app_20171126153848.js
+++ .history/app_20171126153848.js
@@ -9,11 +9,12 @@ var port = 3001;
 var fs = require("fs");
 var students = require(__dirname+'/resources/json/students.json');
 
- 
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.get('/show-students', function(req, res) {
+	// res.status(500).send({data: ["el1", "el2", "el3", "el4"]});
 	res.render('show-students', { students: students } );
 });
 
@@ -21,10 +22,12 @@ app.post('/get-students', function(req, res) {
 	res.setHeader('Content-Type', 'application/json');
 	students.push(req.body);
 	students = JSON.stringify(students);
+	console.log(students);
 	fs.writeFile(__dirname+'/resources/json/students.json', students, 'utf8', function (err) {
 		if (err) {
 			return console.log(err);
 		}
+	
 		console.log("The file was saved!");
 	}); 
     res.send(req.body);
--- .history/resources/js/main_20171126145755.js
+++ .history/resources/js/main_20171126145755.js
@@ -4,6 +4,7 @@ function requestStudents() {
 		type: 'post',
 		data: $("#form").serialize(),
 		success: function(data) {
+			console.log(data);
 			$("form").prepend("<p>"+data.firstname+" "+data.surname+"</p>");
 		},
 		error: function(err) {
